79ea22f5eca79d44bec5834b6789ddb23031a914
Pedro Augusto De Agostini <88171269+PedroAgostini@users.noreply.github.com>
2024-11-15 11:08:23 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,10 +6,8 @@ document.getElementById('chamadoForm').addEventListener('submit', function(event
     var descricao = document.getElementById('descricao').value;
     var importancia = document.getElementById('prioridade').value;
   
-    // Substitua "seu_numero" pelo número de telefone para o qual deseja enviar a mensagem
-    var numeroWhatsApp = "+5514991084904";
+    var numeroWhatsApp = "+5514981228066";
   
-    // Crie o link com os dados do formulário
     var mensagem = "*CHAMADO ABERTO!*" + "%0A*PRIORIDADE: " + encodeURIComponent(importancia) + "*" + "%0A" + "%0ANome: " + encodeURIComponent(nome) + "%0ASetor: " + encodeURIComponent(setor) + "%0ADescrição: " + encodeURIComponent(descricao);
     var linkWhatsApp = "https://api.whatsapp.com/send?phone=" + numeroWhatsApp + "&text=" + mensagem;
   
